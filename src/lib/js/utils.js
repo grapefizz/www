facd1efcb20250f271f1afbae2f9fd76b033e5f1
grapefizz <arikarakushi@proton.me>
2026-01-23 10:02:22 +0100

    changed from dd/mm/yyyy to dd/mm/yy

--- src/lib/js/utils.js
+++ src/lib/js/utils.js
@@ -1,5 +1,5 @@
 export function formatDate(date) {
 	const d = new Date(date);
-	const options = { timeZone: 'UTC', month: '2-digit', day: '2-digit', year: 'numeric' };
+	const options = { timeZone: 'UTC', month: '2-digit', day: '2-digit', year: '2-digit' };
 	return d.toLocaleDateString('en-GB', options);
 }
