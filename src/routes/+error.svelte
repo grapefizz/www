f91ef77a4f0260ec0139a40fa3b2a3387a377d6a
grapefizz <arikarakushi@proton.me>
2026-03-01 00:06:19 +0100

    testing

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,14 @@
 <script>
-	import { page } from '$app/stores';
-	import Piano from '$lib/components/Piano.svelte';
+	import { page } from "$app/stores";
+	import Piano from "$lib/components/Piano.svelte";
 </script>
 
 <main>
 	{#if $page.error}
 		<h1 class="errorCode">{$page.status}</h1>
 		<h2 class="errorMessage">{$page.error.message.toLowerCase()}</h2>
-		<h2>take a moment to play</h2>
-		<Piano />
+		<!-- <h2>take a moment to play</h2>
+		<Piano /> -->
 	{/if}
 </main>
 
