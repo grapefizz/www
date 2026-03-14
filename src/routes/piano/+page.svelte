b1c50e4b9986f9d78b0ffe1e3b1c397e28b7124d
grapefizz <arikarakushi@proton.me>
2026-03-14 11:04:09 +0100

    idk

--- src/routes/piano/+page.svelte
+++ src/routes/piano/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Piano from '$lib/components/Piano.svelte';
+	import Piano from "$lib/components/Piano.svelte";
 </script>
 
 <main>
-  <h1 class="text">piano :></h1>
-  <h2 class="text">take a moment to play</h2>
-  <Piano />
-  <p class="text">press keys on your keyboard or click the piano keys</p>
+	<h1 class="text">piano :></h1>
+	<h2 class="text">take a moment to play</h2>
+	<Piano />
+	<p class="text">press keys on your keyboard or click the piano keys</p>
 </main>
 
 <style>
@@ -19,9 +19,4 @@
 		min-height: calc(100vh - 6rem);
 		padding-bottom: 2rem;
 	}
-
-  text {
-    margin: 0;
-    color: var(--txt);
-  }
 </style>
