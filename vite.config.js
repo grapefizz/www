af7a0f734265c77434d9ae1a731c94b5fd1ab2a7
grapefizz <arikarakushi@proton.me>
2026-05-16 19:48:34 +0200

    gh pages stuff

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
-import { defineConfig } from 'vitest/config';
-import Icons from 'unplugin-icons/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { enhancedImages } from "@sveltejs/enhanced-img";
+import { defineConfig } from "vitest/config";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		sveltekit(),
-		Icons({
-			compiler: 'svelte'
-		})
-	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [
+    enhancedImages(),
+    sveltekit(),
+    svelte(),
+    Icons({
+      compiler: "svelte",
+    }),
+  ],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
