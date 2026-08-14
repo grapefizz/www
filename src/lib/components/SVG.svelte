a3cb48b6602fc25336eb0be7783d19f69965201f
grapefizz <arikarakushi@proton.me>
2026-08-14 21:32:49 +0200

    Revert "new stuff"
    This reverts commit 270eabeb0cf78214f02026fb75ae9fff09cfe3d2.
    
    lesson learned, no more experimenting

--- src/lib/components/SVG.svelte
+++ src/lib/components/SVG.svelte
@@ -1,32 +1,26 @@
 <script>
-	let { svg } = $props();
+  let { svg } = $props();
 
-	const svgs = import.meta.glob(`/src/content/*/*/*.svg`, {
-		import: 'default',
-		eager: true,
-		query: '?raw'
-	});
-
-	function importSVG(svg) {
-		for (const [path, src] of Object.entries(svgs)) {
-			if (path.includes(svg)) {
-				return src;
-			}
-		}
-	}
-
-	const src = $derived(svg ? importSVG(svg) : null);
+  async function importSVG() {
+    const svgs = import.meta.glob(`/src/content/*/*/*.svg`, { query: "?raw" });
+    for (const [path, src] of Object.entries(svgs)) {
+      if (path.includes(svg)) {
+        const res = await src();
+        return res.default;
+      }
+    }
+  }
 </script>
 
 <div class="svg-container">
-	{#if src}
-		{@html src}
-	{/if}
+  {#await importSVG(svg) then src}
+    {@html src}
+  {/await}
 </div>
 
 <style>
-	:global(.svg-container svg) {
-		width: 100%;
-		height: 100%;
-	}
+  :global(.svg-container svg) {
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,184 +1,191 @@
 <script>
-	import '../app.css';
-	import '$lib/assets/fonts/space-mono.css';
-	import '$lib/assets/fonts/space-grotesk.css';
-	import { page } from '$app/state';
-	import LogoAnimated from '$lib/components/LogoAnimated.svelte';
-	import PageHead from '$lib/components/PageHead.svelte';
-	import { fly } from 'svelte/transition';
-	import { prefersReducedMotion } from 'svelte/motion';
-	import { beforeNavigate } from '$app/navigation';
+  import { dev } from "$app/environment";
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
 
-	let { children } = $props();
+  injectAnalytics({ mode: dev ? "development" : "production" });
 
-	const pages = [
-		{ name: 'about', path: '/about' },
-		{ name: 'projects', path: '/projects' },
-		{ name: 'blog', path: '/blog' },
-		{ name: 'pics', path: '/pics' },
-		{ name: 'contact', path: '/contact' }
-	];
+  import "../app.css";
+  import "katex/dist/katex.min.css";
+  import "$lib/assets/fonts/space-mono.css";
+  import "$lib/assets/fonts/space-grotesk.css";
+  import { page } from "$app/state";
+  import Logo from "$lib/components/Logo.svelte";
+  import PageHead from "$lib/components/PageHead.svelte";
+  import { fly } from "svelte/transition";
+  import { prefersReducedMotion } from "svelte/motion";
+  import { beforeNavigate } from "$app/navigation";
 
-	let currPage = $state(page.url.pathname);
-	let prevPage = $state('');
+  let { children } = $props();
 
-	beforeNavigate((navigation) => {
-		if (navigation.to?.url.pathname) {
-			prevPage = currPage;
-			currPage = navigation.to.url.pathname;
-		}
-		console.log(currPage, prevPage);
-	});
+  const pages = [
+    { name: "about", path: "/about" },
+    { name: "projects", path: "/projects" },
+    { name: "blog", path: "/blog" },
+    { name: "pics", path: "/pics" },
+    { name: "contact", path: "/contact" },
+  ];
 
-	function transition(path, out) {
-		const cleanPath = path.replace(/\/$/, '');
-		const cleanPrevPath = prevPage.replace(/\/$/, '');
+  let currPage = $state(page.url.pathname);
+  let prevPage = $state("");
 
-		let currDepth = cleanPath.split('/').length;
-		let prevDepth = cleanPrevPath.split('/').length;
+  beforeNavigate((navigation) => {
+    if (navigation.to?.url.pathname) {
+      prevPage = currPage;
+      currPage = navigation.to.url.pathname;
+    }
+    console.log(currPage, prevPage);
+  });
 
-		const currParent = '/' + cleanPath.split('/')[1];
-		const prevParent = '/' + cleanPrevPath.split('/')[1];
+  function transition(path, out) {
+    const cleanPath = path.replace(/\/$/, "");
+    const cleanPrevPath = prevPage.replace(/\/$/, "");
 
-		let currParentIdx = pages.findIndex((page) => page.path === currParent);
-		let prevParentIdx = pages.findIndex((page) => page.path === prevParent);
+    let currDepth = cleanPath.split("/").length;
+    let prevDepth = cleanPrevPath.split("/").length;
 
-		if (path === '/') {
-			currParentIdx = prevParentIdx;
-			currDepth = 1;
-		}
-		if (prevPage === '/') {
-			prevParentIdx = currParentIdx;
-			prevDepth = 1;
-		}
+    const currParent = "/" + cleanPath.split("/")[1];
+    const prevParent = "/" + cleanPrevPath.split("/")[1];
 
-		let xDiff = currParentIdx - prevParentIdx;
-		let yDiff = currDepth - prevDepth;
+    let currParentIdx = pages.findIndex((page) => page.path === currParent);
+    let prevParentIdx = pages.findIndex((page) => page.path === prevParent);
 
-		if (out) {
-			xDiff *= -1;
-			yDiff *= -1;
-		}
-		if (prefersReducedMotion.current) {
-			xDiff *= 0;
-			yDiff *= 0;
-		}
+    if (path === "/") {
+      currParentIdx = prevParentIdx;
+      currDepth = 1;
+    }
+    if (prevPage === "/") {
+      prevParentIdx = currParentIdx;
+      prevDepth = 1;
+    }
 
-		return {
-			duration: 150,
-			delay: out ? 0 : 50,
-			x: `${xDiff * 20}vh`,
-			y: `${yDiff * 20}vh`
-		};
-	}
+    let xDiff = currParentIdx - prevParentIdx;
+    let yDiff = currDepth - prevDepth;
+
+    if (out) {
+      xDiff *= -1;
+      yDiff *= -1;
+    }
+    if (prefersReducedMotion.current) {
+      xDiff *= 0;
+      yDiff *= 0;
+    }
+
+    return {
+      duration: 150,
+      delay: out ? 0 : 50,
+      x: `${xDiff * 20}vh`,
+      y: `${yDiff * 20}vh`,
+    };
+  }
 </script>
 
 <PageHead
-	title={page.error ? page.status : page.data.meta.title}
-	description={page.error ? page.error.message : page.data.meta.description}
-	type={page.data.meta.type}
-	image={page.data.meta.image}
+  title={page.error ? page.status : page.data.meta.title}
+  description={page.error ? page.error.message : page.data.meta.description}
+  type={page.data.meta.type}
+  image={page.data.meta.image}
 />
 
-<header class:home={page.url.pathname === '/'} data-sveltekit-noscroll>
-	<div class="row">
-		<a class="pfp" href="/" aria-label="homepage">
-			<LogoAnimated skipInitialAnimation={true} />
-		</a>
-		<a href="/"><h1>grapefizz</h1></a>
-	</div>
-	<nav>
-		{#each pages as { name, path }}
-			<a class="nav" href={path}>
-				<span class="arrow">-></span><span class="slash">/</span>{name}
-			</a>
-		{/each}
-	</nav>
+<header class:home={page.url.pathname === "/"} data-sveltekit-noscroll>
+  <div class="row">
+    <a class="pfp" href="/" aria-label="homepage">
+      <Logo
+        --width={page.url.pathname === "/" ? "2rem" : "4rem"}
+        --height={page.url.pathname === "/" ? "2rem" : "4rem"}
+      />
+    </a>
+    <a href="/"><h1>grapefizz</h1></a>
+  </div>
+  <nav>
+    {#each pages as { name, path }}
+      <a class="nav" href={path}>
+        <span class="arrow">-></span><span class="slash">/</span>{name}
+      </a>
+    {/each}
+  </nav>
 </header>
 <div class="container" data-sveltekit-noscroll>
-	{#key page.url.pathname}
-		<div
-			class="transition"
-			in:fly={transition(page.url.pathname, false)}
-			out:fly={transition(page.url.pathname, true)}
-		>
-			{@render children?.()}
-		</div>
-	{/key}
+  {#key page.url.pathname}
+    <div
+      class="transition"
+      in:fly={transition(page.url.pathname, false)}
+      out:fly={transition(page.url.pathname, true)}
+    >
+      {@render children?.()}
+    </div>
+  {/key}
 </div>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 0 4rem;
-		height: 5rem;
-		overflow: hidden;
-		transition: transform 0.1s ease;
-		transform: translateY(0);
-		flex-shrink: 0;
+<style lang="scss">
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5rem;
+    height: 6rem;
+    overflow: hidden;
+    transition: transform 0.1s ease;
+    transform: translateY(0);
+    flex-shrink: 0;
 
-		&.home {
-			transform: translateY(-70%);
-		}
+    &.home {
+      transform: translateY(-70%);
+    }
 
-		.row {
-			display: flex;
-			align-items: center;
-			gap: 1.5rem;
+    .row {
+      display: flex;
+      align-items: center;
+      gap: 1.5rem;
 
-			.pfp {
-				display: flex;
-				width: 2rem;
-				height: 2rem;
-			}
+      .pfp {
+        display: flex;
+      }
 
-			h1 {
-				font-size: 1.375rem;
-				color: var(--txt);
-				margin: 0;
-			}
-		}
+      h1 {
+        font-size: 1.4rem;
+        color: var(--txt);
+        margin: 0;
+      }
+    }
 
-		nav {
-			display: flex;
-			gap: 2.5rem;
+    nav {
+      display: flex;
+      gap: 2.5rem;
 
-			a {
-				font-size: 1.375rem;
-				font-family: 'Space Mono', monospace;
-			}
-		}
-	}
+      a {
+        font-size: 1.4rem;
+        font-family: "Space Mono", monospace;
+      }
+    }
+  }
 
-	.container {
-		height: 100%;
-		display: grid;
-	}
+  .container {
+    height: 100%;
+    display: grid;
+  }
 
-	.transition {
-		grid-column-start: 1;
-		grid-column-end: 2;
-		grid-row-start: 1;
-		grid-row-end: 2;
-		min-width: 0;
-	}
+  .transition {
+    grid-column-start: 1;
+    grid-column-end: 2;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    min-width: 0;
+  }
 
-	@media (max-width: 850px) {
-		header {
-			padding: 0 1.5rem;
-			gap: 1.5rem;
+  @media (max-width: 850px) {
+    header {
+      padding: 0 1.5rem;
+      gap: 1.5rem;
 
-			nav {
-				gap: 1.5rem;
-			}
-		}
-	}
+      nav {
+        gap: 1.5rem;
+      }
+    }
+  }
 
-	@media (max-width: 700px) {
-		header nav {
-			display: none;
-		}
-	}
+  @media (max-width: 700px) {
+    header nav {
+      display: none;
+    }
+  }
 </style>
